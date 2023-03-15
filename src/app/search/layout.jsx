4f1afa9dfd7b3eee407ef3b69adeb3bfb60329ca
Rainbow <kenesou@gmail.com>
2023-03-15 16:37:13 +1300

    move footer from page to layout under src folder and create search and web forder with page respecitively

--- src/app/search/layout.jsx
+++ src/app/search/layout.jsx
@@ -1,12 +1,12 @@
 import './globals.css'
-import Footer from '@/components/Footer';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="relative min-h-screen">
         {children}
 
-        <Footer />
+        
         </body>
     </html>
   )
