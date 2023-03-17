6f679e21f1ac73ed88682d96cbc6da84057f76ee
Rainbow <kenesou@gmail.com>
2023-03-17 22:40:45 +1300

    fetch data from google search API and show the link and title

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,11 +1,29 @@
 import SearchHeader from '@/components/SearchHeader'
+import { useSearchParams } from 'next/navigation';
 import React from 'react'
 
-export default function WebSearchPage() {
+export default async function WebSearchPage({searchParams}) {
+  
+  
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+
+  const data = await response.json();
+  const searchResults = data.items;
   return (
     <>
       <SearchHeader/>
-      <div>WebSearchPage</div>
+      {searchResults && 
+        searchResults.map((data) => (
+          <div className='p-3'>
+            <p className='text-sm'>{data.link}</p>
+            <h1 className='text-blue-600 text-xl hover:underline'
+              ><a href={data.link}>{data.title}</a></h1>
+          </div>
+          
+
+        ))}
     </>
     
   )
