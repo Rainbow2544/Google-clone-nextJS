904f5cc56e4b74766ac343375fc8c68844c6cace
Rainbow <kenesou@gmail.com>
2023-03-18 00:37:41 +1300

    finish display the searchResults in web

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default async function WebSearchPage({searchParams}) {
   
-  
+  //await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
@@ -16,39 +16,44 @@ export default async function WebSearchPage({searchParams}) {
 
   const data = await response.json();
   const searchResults = data.items;
-  console.log(searchResults);
+  console.log(data);
 
+  //when there is no results
   if(!searchResults){
     return(
       <div className="flex flex-col justify-center items-center pt-10">
-        <h1 className="text-3xl mb-4">No results found</h1>
+        <h1 className="text-2xl mb-4">Your search - {searchParams.searchTerm} - did not match any documents.</h1>
         <p className="text-lg">
-          Try searching for something else or go back to the{" "} 
+          Try to search for something else or go back to the{" "} 
           <Link href="/" className="text-blue-500">
              Home 
           </Link>
           {" "} page.
         </p>
+        <img src='https://timgm.eprice.com.tw/tw/nb/img/2021-11/12/5678235/eprice_1_cc8e198aa31956a3e7254f9962d06bfc.jpg'/>
       </div>
     );
   }
   return (
-    <>
-      
+    <div className='sm:pl-[5%] md:pl-[14%] lg:pl-52'>
+      <p className=' mt-3 text-sm text-gray-600'>
+        About {data.searchInformation?.formattedTotalResults} results
+        ({data.searchInformation?.formattedSearchTime} seconds)
+      </p>
       {searchResults && 
         searchResults.map((data) => (
-          <div className='p-3 mx-[200px] mt-3 w-[600px]'>
+          <div className=' my-6 w-[650px]'>
             
             <h1 className='text-blue-700 text-lg hover:underline truncate '
               ><a href={data.link}>{data.title}</a></h1>
               <p className='text-xs mb-2'>{data.link}</p>
-              <p className='text-gray-600'>{data.snippet}</p>
+              <p className='text-gray-600 text-sm'>{data.snippet}</p>
               
           </div>
           
 
         ))}
-    </>
+    </div>
     
   )
 }
